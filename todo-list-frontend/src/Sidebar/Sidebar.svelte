6a5a75da5d11fc22a8eb7ffb1435b30bda84cc6d
Sanideg <degasperi.daniela@gmail.com>
2024-08-27 19:38:20 +0800

    working on the website!
    Co-authored-by: Moritz Bergemann <Moritz-Bergemann@users.noreply.github.com>

---
+++ todo-list-frontend/src/Sidebar/Sidebar.svelte
@@ -0,0 +1,54 @@
+<div class="sidebarbar">
+    <div class="sidebar-title">
+        <h2>To-Do List</h2>
+    </div>
+    <div class="sidebar-links">
+    <a href="#a">To-Do List 1</a>
+</div>
+</div>
+
+<style>
+    .sidebarbar {
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        height: 100%;
+        width: 27%;
+        padding-left: 20px;
+        padding-right: 20px;
+        margin-left: -10px;
+        margin-top: 11px;
+        background-color: #0081A7;
+        box-shadow: 2px 2px 10px rgba(0, 0, 0,0.3);
+        border-radius: 25px;
+        position: fixed;
+        overflow-x: hidden;
+        margin-left: 0;
+        transition:  0.7s;
+    }
+    .sidebar-title {
+        color:#F07167;
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;  
+        text-align: center;
+        font-size: 30px;
+    }
+    .sidebar-links {
+        color: #0081A7;
+        background-color: #FDFCDC;
+        border-radius: 25px;
+        padding: 5px;
+        text-align: center;
+        margin-left: 42px;
+        margin-top: 0;
+        width: 300px;
+    }
+    .sidebar-links:hover {
+        border:solid 5px #00AFB9;
+        margin-left: 37px;
+        margin-top: -5px;
+    }
+    .sidebar-links:active {
+        background-color: #FDFCDC;
+        border:solid 5px #F07167;
+        margin-left: 37px;
+        margin-top: -5px;
+    }
+</style>
--- todo-list-frontend/src/routes/+page.svelte
+++ todo-list-frontend/src/routes/+page.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
 import { onMount } from "svelte";
+import Sidebar from "../Sidebar/Sidebar.svelte";
 
 let pingResponseMessage: string;
 let pingResponseCount: number;
 
 let todos: TodoItem[] = [];
 let newTodoDescription = "";
+
+								// Parameter
+async function checkUncheckTodo(todo: TodoItem) {
+	
+}
 
 async function getTodos() {
 	const response = await fetch("http://localhost:3000/todo-item");
@@ -39,24 +45,120 @@ async function addTodoButtonclick() {
 onMount(() => {
 	getTodos();
 });
+
+let active = false;
+
+let sidebar = false;
+function togglesidebar() {
+	sidebar = !sidebar;
+};
+
 </script>
+<div>
+	<button class="sidebarbutton" on:click={togglesidebar}></button>
+	{#if sidebar}
+		<Sidebar></Sidebar>
+	{/if}
+</div>
 
-<h1>POGGERS To-do list</h1>
+<div class="poggers-title">
+<h1>Poggers Checklist</h1></div>
 
-<label for="new-todo-description">Description</label>
-<input name="new-todo-description" bind:value={newTodoDescription}/>
-<button on:click={addTodoButtonclick}>Add new todo</button>
 
-{#each todos as todo}
+<div class="checklist-container">
+	<div class="checklist-title"><h2>Checklist Title</h2></div>
+	{#each todos as todo}
 	<div class="todo-item">[{todo.isDone ? "X" : " "}] {todo.description}</div>
-{/each}
+{/each}	
+		<input class="new-todo-input" name="new-todo-description" type="text" bind:value={newTodoDescription}/>
+		<button class="todo-input-button" on:click={addTodoButtonclick}>+ Add New Task</button>
+	</div>
 
 
 <style>
+	:global(body) {
+		background-color: #FED9B7;
+	}
+	.poggers-title {
+		text-align: center;
+		color:#F07167;
+		font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+		font-size: 30px;
+		margin-top: -50px ;
+
+	}
+	.checklist-container {
+		background-color: #FDFCDC;
+		border-radius: 25px;
+		width: 1000px;
+		padding-bottom: 50px;
+		padding-top: 20px;
+		height: auto;
+		margin-left: 17%;
+		margin-bottom: 50%;
+		margin-top: -20px;
+	}
+	.checklist-title {
+		text-align: center;
+		background-color: #F07167;
+		width:1000px;
+		color: #FFFFFF;
+		font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+		font-size: 23px;
+		box-shadow: 2px 2px 10px rgba(0, 0, 0,0.3);
+	}
+	.new-todo-input {
+		width: 400px;
+		height: 18px;
+		padding: auto;
+		margin-left: 10px;
+		margin-right: 10px;
+		background-color: #FDFCDC;
+		border: none;
+		border-bottom: solid 2px #0081A7;
+		font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+		color:#0081A7;
+		font-size: 18px;
+	}
+	.todo-input-button {
+		background-color: rgba(0, 0, 0, 0);
+		color: rgba(0, 129, 168, 0.3);
+		border: none;
+		font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+		font-size: 18px;
+		text-align: center;
+		transition-duration: 0.1s;
+	}
+	.todo-input-button:hover {
+		transition-duration: 0.1s;
+		color: #0081A7;
+	}
 	.todo-item {
-		background-color: cornflowerblue;
 		width: 200px;
 		margin: 5px;
 		padding: 10px;
+		font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+		color:#0081A7;
+		font-size: 18px;
 	}
+
+	.todo-item:hover {
+		background-color: #f0706765;
+		border-radius: 25px;
+	}
+
+	.sidebarbutton {
+		background-color: #F07167;
+		font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+		width: 100px;
+		height: 70px;
+		border: none;
+		margin-left: -10px;
+		margin-top: 11px;
+		border-top-right-radius: 25px;
+		border-bottom-right-radius: 25px;
+		display: absolute;
+		position: absolute;
+	}
+
 </style>
